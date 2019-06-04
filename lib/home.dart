3fcbc83f051b3700c2f0cf9adaafa10aa786d31a
hao <1451295544@qq.com>
2019-06-04 13:55:42 +0800

    首页搭建

--- lib/home.dart
+++ lib/home.dart
@@ -1,29 +1,70 @@
 import 'package:flutter/material.dart';
 
-class homePage extends StatelessWidget {
-  homePage() : super();
+class HomePage extends StatelessWidget {
+  HomePage() : super();
+
+  static const String _title = 'flutter shop';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: TabPage(),
+    );
+  }
+}
+
+class TabPage extends StatefulWidget {
+  TabPage({Key key}) : super(key: key);
+
+  @override
+  _MyTabStetefulWidgetState createState() => _MyTabStetefulWidgetState();
+}
+
+class _MyTabStetefulWidgetState extends State<TabPage> {
+  int _selectIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Message',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Me',
+      style: optionStyle,
+    )
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hahaa"),
+        title: const Text("首页"),
       ),
-      body: new Container(
-        padding: const EdgeInsets.all(32.0),
-        child: new Row(
-          children: [
-            new Container(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: new Text("123"),
-            ),
-            new Icon(
-              Icons.star,
-              color: Colors.red,
-            ),
-            new Text("41")
-          ],
-        ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("home")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message), title: Text("message")),
+          BottomNavigationBarItem(icon: Icon(Icons.near_me), title: Text("me"))
+        ],
+        currentIndex: _selectIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -24,43 +24,48 @@ class MySplashPage extends StatefulWidget {
 
   @override
   _SplashPageState createState() => _SplashPageState();
+
 }
 
 class _SplashPageState extends State {
-  Timer timer;
+//  Timer timer;
+
+
+  @override
+  void dispose() {
+    //页面销毁时
+//    timer.cancel();
+    super.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     //首次调用,只调用一次
-    timer =  new Timer(const Duration(milliseconds: 1500),(){
-      //定时器完成时的回调
-      Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
-          builder: (BuildContext context) => new homePage()), (//跳转到主页
-          Route route) => route == null);
-    });
-//    new Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
-//      Navigator.of(context).pushAndRemoveUntil(
-//          new MaterialPageRoute(
-//              builder: (BuildContext context) => new homePage()),
-//          ( //跳转到主页
-//                  Route route) =>
-//              route == null);
-//      return true;
+//    timer = new Timer(const Duration(milliseconds: 1500), () {
+//      //定时器完成时的回调
+//      Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(
+//          builder: (BuildContext context) => new homePage()), ( //跳转到主页
+//          Route route) => route == null);
 //    });
-  }
-
-  @override
-  void dispose() {
-    //页面销毁时
-    timer.cancel();
-    super.dispose();
+    new Future.delayed(const Duration(seconds: 2, milliseconds: 500), () {
+      Navigator.of(context).pushAndRemoveUntil(
+          new MaterialPageRoute(
+              builder: (BuildContext context) => new HomePage()),
+              ( //跳转到主页
+              Route route) =>
+          route == null);
+      return true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Image.asset("images/splash.jpg"),
+      body: new Center(
+        child: new Image.asset("images/splash.jpg"),
+
+      )
     );
   }
 }
